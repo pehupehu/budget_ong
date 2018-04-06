5326d37fd5a4a1d96288c9a5639815feeaa16afb
Cedric Peru <cperu@hexaglobe.com>
2018-04-06 12:36:55 +0200

    first commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
+}

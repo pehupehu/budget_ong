6dc93526cb841d4773da788c7eab6a796dbd3714
Cedric Peru <cperu@hexaglobe.com>
2018-04-10 14:27:37 +0200

    Delegation, Project entities

---
+++ src/Entity/Delegation.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Table(name="delegations")
+ */
+class Delegation
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=5, unique=true)
+     * @var string
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Delegation
+     */
+    public function setId(int $id): Delegation
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     * @return Delegation
+     */
+    public function setCode(string $code): Delegation
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Delegation
+     */
+    public function setName(string $name): Delegation
+    {
+        $this->name = $name;
+        return $this;
+    }
+}

c5e602418b83141757055200821366a18d3e3dc2
Cedric Peru <cperu@hexaglobe.com>
2018-04-12 16:11:29 +0200

    css

--- public/ppbox.js
+++ public/ppbox.js
@@ -13,25 +13,31 @@ function ppboxInit() {
         window.location.href = url;
     };
 
-    ppbox.confirm = function(title, text, options = null) {
-        ppbox.createDialog(title, text, options);
+    ppbox.confirm = function(title, text, theme = 'dark', options) {
+        ppbox.createDialog(title, text, theme = 'dark', options);
     };
 
-    ppbox.alert = function(title, text, options) {
-        ppbox.createDialog(title, text, options);
+    ppbox.alert = function(title, text, theme = 'dark', options) {
+        ppbox.createDialog(title, text, theme = 'dark', options);
     };
 
-    ppbox.createDialog = function(title, text, options = null) {
+    ppbox.createDialog = function(title, text, theme = 'dark', options) {
         dialog_inc = dialog_inc + 1;
         $(document.body).append('<div id="ppboxdialog' + dialog_inc + '" title="' + title + '">' + text + '</div>');
 
         // Bootstrap dialog
+        if (options === undefined) {
+            options = {};
+        }
 
-        // 'classes': {
-        //     "ui-dialog": "ui-corner-allaaaaaaaa",
-        //         "ui-dialog-titlebar": "ui-corner-allaaaaaaaaaaaaabbbbbb",
-        // }
+        console.log(theme);
 
+        if (options.classes === undefined) {
+            options.classes = {
+                "ui-dialog": ("ui-corner-all ui-dialog-" + theme),
+                "ui-dialog-titlebar": ("ui-corner-all ui-dialog-titlebar-" + theme),
+            }
+        }
 
         // Bootstrap buttons
         // 'class': 'btn btn-sm btn-info',
@@ -46,7 +52,7 @@ function ppboxInit() {
 let ppbox = ppboxInit();
 
 // ppbox.alert('pouet', 'test 1', { buttons: { 'revenir': function() { window.location.href='tata.php' }, 'revenir2': function() { window.location.href='tata2.php' } }});
-// ppbox.alert('pouet', 'test 2', { buttons: { 'revenir': function() { $(this).dialog('close') } } });
+ppbox.alert('pouet', 'test 2', 'dark', { buttons: { 'revenir': function() { $(this).dialog('close') } } });
 
 // ppbox.confirm('pouet', 'test 1', { buttons: { 'revenir': function() { $(this).dialog('close') } }});
 // ppbox.confirm('Confirm', 'Are you sure ?', {});

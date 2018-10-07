9fed71490e9a295b4761f6ee942af6f070df1d7f
Cédric PERU <cedric.peru@gmail.com>
2018-10-07 23:51:47 +0200

    Dev

--- public/js/ppbox-init.js
+++ public/js/ppbox-init.js
@@ -1,4 +1,5 @@
-let ppboxconfirm = $('.ppboxconfirm');
+let ppboxconfirm = $('.ppboxconfirm'),
+    ppboxalert = $('.ppboxalert');
 
 ppboxconfirm.on('click', function () {
     let id = $(this).data('ppbox-id'),
@@ -11,4 +12,17 @@ ppboxconfirm.on('click', function () {
         button3 = $(this).data('ppbox-button3');
 
     PPbox.confirm(id, title, body, theme, width, button1, button2, button3)
+});
+
+ppboxalert.on('click', function () {
+    let id = $(this).data('ppbox-id'),
+        title = $(this).data('ppbox-title'),
+        body = $(this).data('ppbox-body'),
+        theme = $(this).data('ppbox-theme'),
+        width = $(this).data('ppbox-width'),
+        button1 = $(this).data('ppbox-button1'),
+        button2 = $(this).data('ppbox-button2'),
+        button3 = $(this).data('ppbox-button3');
+
+    PPbox.alert(id, title, body, theme, width, button1, button2, button3)
 });
